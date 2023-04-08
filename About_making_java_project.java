d86c3297dba139239ffb1ae96849ec07cb821eeb
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-04-08 22:35:55 +0900

    Add files via upload

---
+++ About_making_java_project.java
@@ -0,0 +1,14 @@
+package test123;
+
+public class HelloJava {
+	
+	public static void main(String [] args)
+	{
+		System.out.println("Hello Java");
+	}
+
+}
+
+
+// File -> New - > Project 에서 프로젝트 생성
+// 후, 자바 소스가 위치하는 폴더인 src에서 class 생성.
