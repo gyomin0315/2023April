3e61a1cb0fed53630b2d14a330f120c56abf273d
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-04-09 22:40:03 +0900

    Add files via upload

---
+++ Character_data_type.java
@@ -0,0 +1,55 @@
+
+public class Character_data_type {
+	
+	public static void main(String[]args)
+	{
+		char a = 'A';
+		
+		System.out.println("a:"+a);
+		
+		int b = a;
+		
+		//System.out.println("b:"+b);
+		
+		//System.out.println("b:"+b);
+		
+		//System.out.println("b:"+b);
+		
+		//System.out.print(b);
+		
+		System.out.println(b);
+		
+		System.out.println(b);
+		
+		
+		
+	
+		
+		char c = 66;
+		System.out.println("c:"+c);
+		
+		int d = a+b;
+		System.out.println("d:"+d);
+		
+		
+	
+		
+		
+		
+	}
+
+}
+
+// println은 그냥 print와 다르게, \n의 역할을 뒤에 알아서 붙여주는듯. 문자열과 변수를 함께 사용할 수 있고, +기호로 둘을 붙임. 
+
+// System.out.println하고, 안에 들어갈 데이터형을 정해야하는데, int,float,char형 등등 ... int 형임에도, char나 다른 형을 써도 출력은 잘 됨. But, 데이터형은 맞춰주는게 좋을듯. 
+
+//출력 결과
+/*
+a:A
+65
+65
+c:B
+d:130
+ * 
+ */
