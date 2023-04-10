b2965feaf77cbe9652da6e4c5d124e4bcf1d1b2d
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-04-10 21:43:56 +0900

    Add files via upload

---
+++ Float_and_double_data_type.java
@@ -0,0 +1,34 @@
+
+public class Float_and_double_data_type {
+	
+	public static void main(String[]args)
+	{
+		
+		float f = 3.141592653589793f;
+		double d = 3.141592653589793d;
+		
+		System.out.println("float : " +f);
+		
+		System.out.println("double : "+d);
+		
+		double d2 = 3.141592653589793;
+		
+		System.out.println("double : " + d2);
+		
+	}
+
+}
+
+//출력 결과
+/*
+float : 3.1415927
+double : 3.141592653589793
+double : 3.141592653589793
+
+ */
+
+// float는 뒤에 f나 F를, double은 뒤에 d나 D를 붙여줌. 
+// double이 기본 실수 처리 자료형이기 떄문에, double은 d 생략 가능. 
+
+// C와 마찬가지로, float는 4byte, double은 8 byte사용. 
+
