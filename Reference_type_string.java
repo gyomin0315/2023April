2979158078a2afd467bd16174f3109d633675085
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-04-11 21:41:54 +0900

    Add files via upload

---
+++ Reference_type_string.java
@@ -0,0 +1,34 @@
+
+public class Reference_type_string {
+	
+	public static void main(String[]args)
+	{
+		
+		String name;
+		
+		name = "홍길동";
+		
+		String name2 = "구교민";
+		
+		String name3 = null;
+		String name4 = "";
+		
+		System.out.println("name :" + name);
+		System.out.println("name2 :" + name2);
+		System.out.println("name3 :" + name3);
+		System.out.println("name4 :" + name4);
+				
+	}
+
+}
+
+//출력 결과
+/*
+name :홍길동
+name2 :구교민
+name3 :null
+name4 :
+
+ */
+
+// 참조 자료형 : 값을 직접 저장하는게 아닌, 메모리에 저장된 위치정보를 저장. 앞에 있었던 기본자료형 외의 모든게 참조자료형. 
