9d1916893998ad1c5b93dd6265b59d00f1917acb
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-04-15 16:19:00 +0900

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,48 @@
+
+public class Operator {
+	
+	
+	
+	public static void main(String[]args)
+	{
+		int number1 = 10;
+		System.out.println("number1 = 10 ->"+ number1);
+		
+		number1 +=10; // = number1 = number1 + 10
+		
+		System.out.println("number1 = 10 ->"+ number1);
+		
+		number1 -=10;
+		
+		System.out.println("number1 = 10 ->"+ number1);
+		
+		number1 *=2;
+		
+		System.out.println("number1 = 10 ->"+ number1);
+		
+		number1 /=2;
+		
+		System.out.println("number1 = 10 ->"+ number1);
+		
+		number1 %= 3; // 나머지
+		
+		System.out.println("number1 = 10 ->"+ number1);
+		
+		
+
+	}
+	
+}
+
+//출력 결과
+/*
+number1 = 10 ->10
+number1 = 10 ->20
+number1 = 10 ->10
+number1 = 10 ->20
+number1 = 10 ->10
+number1 = 10 ->1
+ */
+
+
+
