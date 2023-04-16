630bb0e15f99d96cb8e84550f74763f76c016942
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-04-16 13:33:18 +0900

    Add files via upload

---
+++ Operator_2.java
@@ -0,0 +1,56 @@
+
+public class Operator_2 {
+	
+	
+	
+	public static void main(String[]args)
+	{
+		int a = 5+5;
+		System.out.println("5+5 = " + a);
+		
+		int b = a-5;
+		System.out.println("10-5 = " + a);
+		
+		int c = b*2;
+		System.out.println("5*2 = " + a);
+		
+		int d = c/2;
+		System.out.println("10 / 2 = " + a);
+		
+		int e = c%3;
+		System.out.println("10 % 3 = " + a);
+		
+		int f = 5;
+		
+		System.out.println("+f = " + +f);
+		System.out.println("-f = " + -f);
+		
+		int g = -5;
+		
+		System.out.println("+g = " + +g);
+		System.out.println("-g = " + -g);
+		
+		
+		
+		
+
+	}
+	
+}
+
+//출력 결과
+/*
+ * 5+5 = 10
+10-5 = 10
+5*2 = 10
+10 / 2 = 10
+10 % 3 = 10
++f = 5
+-f = -5
++g = -5
+-g = 5
+
+*/
+
+
+
