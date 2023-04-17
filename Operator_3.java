8ef42ce9c57e0446aaef65b5b7298fb8dce50c45
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-04-17 22:32:25 +0900

    Add files via upload

---
+++ Operator_3.java
@@ -0,0 +1,45 @@
+
+public class Operator_3 {
+	
+	
+	
+	public static void main(String[]args)
+	{
+		int a = 10;
+		int b = 10;
+		
+		++a;
+		b++;
+		
+		System.out.println("a : "+ a);
+		System.out.println("b : "+ b);
+		
+		int c = 10;
+		int d = ++c;
+		
+		System.out.println("d: " + d);
+		
+		int e = 10;
+		int f = e++;
+		
+		System.out.println("f: " + f);
+		
+
+	}
+	
+}
+
+//출력 결과
+/*
+a : 11
+b : 11
+d: 11
+f: 10
+
+
+*/
+
+// 전위연산, 후위연산은 C와 똑같음. 
+
+
+
