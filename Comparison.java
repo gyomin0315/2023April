1f68ee6c4dcc96ddc6cd76dbbeddd18d4bf933e0
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-04-18 22:22:22 +0900

    Add files via upload

---
+++ Comparison.java
@@ -0,0 +1,38 @@
+
+public class Comparison {
+	
+	
+	
+	public static void main(String[]args)
+	{
+		int a = 10;
+		int b = 5;
+		
+		System.out.println(a>b);
+		System.out.println(a>=b);
+		System.out.println(a<b);
+		System.out.println(a<=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+	
+	}
+	
+}
+
+//출력 결과
+/*
+true
+true
+false
+false
+false
+true
+
+
+
+*/
+
+// C와의 차이는 , 자바는 boolean 자료형이 있어서 참, 거짓이 true, false로 나온다. (C는 거짓은 0, 참은 0이 아닌 모든 수)
+
+
+
