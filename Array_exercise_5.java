8baa206f55fcaaf3e886b3ab872b302ef733a634
gyomin0315 <98148630+gyomin0315@users.noreply.github.com>
2023-05-12 12:02:33 +0900

    Add files via upload

---
+++ Array_exercise_5.java
@@ -0,0 +1,55 @@
+package myPackage;
+
+public class Array_exercise_5 {
+	
+	public static void main(String args[])
+	{
+		// simple for문. 값을 읽을 수만 있고, 변경할 수 없다. 
+		
+		int arr[] = {1,2,3,4,5};
+		
+		// 기존에는 배열의 출력을 이렇게 했다면,
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i] + " ");
+		}
+		
+		System.out.println();
+		
+		// simple for문을 쓰게 되면, 아래와 같이 간편하게 쓸 수 있음.
+		
+		for(int number : arr) // 앞쪽은 배열을 넣을 변수 선언, 뒤는 반복 가능한 배열. 이 배열의 첫번째 요소부터 차례대로 number에 대입된다. 
+		{
+			System.out.print(number + " ");
+		}
+		
+		System.out.println();
+		String [] names = {"홍길동","이순신","김유신"};
+		int score[] = {90,80,70};
+		
+		int i=0;
+		
+		
+		for(String names2 : names)
+		{
+			System.out.println(names2 + ":" + score[i]);
+			i++;
+			
+		}
+		
+		
+		
+	
+	}
+
+}
+
+//출력 결과
+/*
+1 2 3 4 5 
+1 2 3 4 5 
+홍길동:90
+이순신:80
+김유신:70
+ */
